b9dfaf864991fb1086deff83ac7a2ae48615a591
Anand Jha <cyberaj123@gmail.com>
2020-12-17 01:41:04 +0530

    all done but not styling

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
-var inputText = document.querySelector("#txt-input");
+var txtInput = document.querySelector("#txt-input");
 var translatebtn = document.querySelector("#btn-translate");
-var outputTxt = document.querySelector("#output");
+var txtOutput = document.querySelector("#output");
 
 //for writing value
 //outputTxt.innerHTML = "anand";
 
 //for writing value
 //.value
+
+let serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(input) {
+  return serverUrl + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
 function clickHandler() {
-  outputTxt.innerText = "kkdjjdhhhs " + inputText.value;
-  console.log("output", inputText.value);
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      txtOutput.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
 translatebtn.addEventListener("click", clickHandler);
